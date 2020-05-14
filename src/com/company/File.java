50e58d3e52316e8392f57728a5e3f76dae2b57e5
AndreiBy <andreialshuk@gmail.com>
2020-05-14 13:56:47 +0300

    Project creation Add Class Enum FileStatus Add Class File Add Class FileController

---
+++ src/com/company/File.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class File {
+    String nameFile;
+    double sizeFile;
+    String creatorFile;
+
+    public File(String nameFile, double sizeFile, String creatorFile) {
+        this.nameFile = nameFile;
+        this.sizeFile = sizeFile;
+        this.creatorFile = creatorFile;
+    }
+}
---
+++ src/com/company/FileController.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileController {
+Scanner scanner;
+ArrayList <File> File;
+
+    FileController(){
+    scanner = new Scanner(System.in);
+    File = new ArrayList<>();
+}
+
+
+}
+
